46d3a79f69d63750ef93a62b0d892fdf8802e4da
codingwithvb <72670340+codingwithvb@users.noreply.github.com>
2021-03-03 07:11:48 -0500

    Add files via upload

---
+++ factoring.c
@@ -0,0 +1,248 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    printf("This code is to find the factors of a particular POSITIVE INTEGER. The number will need to be less than or equal to 150, or else it might give you the incorrect results. You can also find if a number is prime or not, if it's factors are 1 and itself then it is a prime number, if it has more factors than it is composite.\n");
+    int number = get_int("What is your number? \n");
+     if(151<=number){
+        printf("This number is greater than 150! The following results may not be accurate! \n");
+    }
+    if(number % 1 == 0){
+        printf("1\n");
+    }
+    if(number % 2 == 0){
+        printf("2\n");
+    }
+    if(number % 3 == 0){
+        printf("3\n");
+    }
+    if(number % 4 == 0){
+        printf("4\n");
+    }
+    if(number % 5 == 0){
+        printf("5\n");
+    }
+    if(number % 6 == 0){
+        printf("6\n");
+    }
+    if(number % 7 == 0){
+        printf("7\n");
+    }
+    if(number % 8 == 0){
+        printf("8\n");
+    }
+    if(number % 9 == 0){
+        printf("9\n");
+    }
+    if(number % 10 == 0){
+        printf("10\n");
+    }
+    if(number % 11 == 0){
+        printf("11\n");
+    }
+    if(number % 12 == 0){
+        printf("12\n");
+    }
+    if(number % 13 == 0){
+        printf("13\n");
+    }
+    if(number % 14 == 0){
+        printf("14\n");
+    }
+    if(number % 15 == 0){
+        printf("15\n");
+    }
+    if(number % 16 == 0){
+        printf("16\n");
+    }
+    if(number % 17 == 0){
+        printf("17\n");
+    }
+    if(number % 18 == 0){
+        printf("18\n");
+    }
+    if(number % 19 == 0){
+        printf("19\n");
+    }
+    if(number % 20 == 0){
+        printf("20\n");
+    }
+    if(number % 21 == 0){
+        printf("21\n");
+    }
+    if(number % 22 == 0){
+        printf("22\n");
+    }
+    if(number % 23 == 0){
+        printf("23\n");
+    }
+    
+    if(number % 24 == 0){
+        printf("24\n");
+    }
+    if(number % 25 == 0){
+        printf("25\n");
+    }
+    if(number % 26 == 0){
+        printf("26\n");
+    }
+    if(number % 27 == 0){
+        printf("27\n");
+    }
+    if(number % 28 == 0){
+        printf("28\n");
+    }
+    if(number % 29 == 0){
+        printf("29\n");
+    }
+    if(number % 30 == 0){
+        printf("30\n");
+    }
+    if(number % 31 == 0){
+        printf("31\n");
+    }
+    if(number % 32 == 0){
+        printf("32\n");
+    }
+    if(number % 33 == 0){
+        printf("33\n");
+    }
+    if(number % 34 == 0){
+        printf("34\n");
+    }
+    if(number % 35 == 0){
+        printf("35\n");
+    }
+    if(number % 36 == 0){
+        printf("36\n");
+    }
+    if(number % 37 == 0){
+        printf("37\n");
+    }
+    if(number % 38 == 0){
+        printf("38\n");
+    }
+    if(number % 39 == 0){
+        printf("39\n");
+    }
+   if(number % 40 == 0){
+        printf("40\n");
+    }
+    if(number % 41 == 0){
+        printf("41\n");
+    }
+    if(number % 42 == 0){
+        printf("42\n");
+    }
+    if(number % 43 == 0){
+        printf("43\n");
+    }
+    if(number % 44 == 0){
+        printf("44\n");
+    }
+    if(number % 45 == 0){
+        printf("45\n");
+    }
+    if(number % 46 == 0){
+        printf("46\n");
+    }
+    if(number % 47 == 0){
+        printf("47\n");
+    }
+    if(number % 48 == 0){
+        printf("48\n");
+    }
+    if(number % 49 == 0){
+        printf("49\n");
+    }
+    if(number % 50 == 0){
+        printf("50\n");
+    }
+    if(number % 51 == 0){
+        printf("51\n");
+    }
+    if(number % 52 == 0){
+        printf("52\n");
+    }
+    if(number % 53 == 0){
+        printf("53\n");
+    }
+    if(number % 54 == 0){
+        printf("54\n");
+    }
+    if(number % 55 == 0){
+        printf("55\n");
+    }
+    if(number % 56 == 0){
+        printf("56\n");
+    }
+    if(number % 57 == 0){
+        printf("57\n");
+    }
+    if(number % 58 == 0){
+        printf("58\n");
+    }
+    if(number % 59 == 0){
+        printf("59\n");
+    }
+    if(number % 60 == 0){
+        printf("60\n");
+    }
+    if(number % 61 == 0){
+        printf("61\n");
+    }
+    if(number % 62 == 0){
+        printf("62\n");
+    }
+    if(number % 63 == 0){
+        printf("63\n");
+    }
+    if(number % 64 == 0){
+        printf("64\n");
+    }
+    if(number % 65 == 0){
+        printf("65\n");
+    }
+    if(number % 66 == 0){
+        printf("66\n");
+    }
+    if(number % 67 == 0){
+        printf("67\n");
+    }
+    if(number % 68 == 0){
+        printf("68\n");
+    }
+    if(number % 69 == 0){
+        printf("69\n");
+    }
+    if(number % 70 == 0){
+        printf("70\n");
+    }
+    if(number % 71 == 0){
+        printf("71\n");
+    }
+    if(number % 72 == 0){
+        printf("72\n");
+    }
+    if(number % 73 == 0){
+        printf("73\n");
+    }
+   if(number % 74 == 0){
+        printf("74\n");
+    } 
+    if(number % 75 == 0){
+        printf("75\n");
+    }
+    if(number > 75)
+    {
+        printf("%i\n",number);
+    }
+    if(number == 0)
+    {
+        printf("0 is not considerd to be a POSITIVE INTEGER, try again with a POSITIVE INTEGER!\n");
+    }
+    if(number == 1)
+    {
+        printf(" 1 is neither a prime or composite number!\n");
+    }
+}
